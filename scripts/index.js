b0ed9a1c9e99e7cc216465c941cde8dbfefaf277
Michu0112 <66910540+Michu0112@users.noreply.github.com>
2021-02-28 13:20:11 +0100

    add responsive menu

---
+++ scripts/index.js
@@ -0,0 +1,17 @@
+const navBar = document.querySelector('.nav-bar');
+const menuBtn = document.querySelector('.menu-btn');
+const body = document.querySelector('body');
+let menuOpen = false;
+
+menuBtn.addEventListener('click', () =>{
+   if(!menuOpen){
+      menuBtn.classList.add('open');
+      menuOpen = true;
+      navBar.style.right = 0;
+   }
+   else{
+      menuBtn.classList.remove('open');
+      menuOpen = false;
+      navBar.style.right = '-100%';
+   }
+});
